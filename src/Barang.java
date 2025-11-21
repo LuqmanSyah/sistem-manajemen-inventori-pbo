30dde4320edb6b52393c454019ab7d679d842ff1
Luqman <89076043+LuqmanSyah@users.noreply.github.com>
2025-11-21 09:42:27 +0700

    Fitur baru

--- src/Barang.java
+++ src/Barang.java
@@ -1,17 +1,26 @@
-public class Barang {
+class Kategori {
+  protected String kategori;
+
+  public Kategori() {}
+}
+
+public class Barang extends Kategori {
   private String id;
   private String name;
   private int qty;
 
-  public void setBarang(String id, String name, int qty) {
+  public void setBarang(String id, String name, int qty, String k) {
     this.id = id;
     this.name = name;
     this.qty = qty;
+    kategori = k;
   }
 
   public void getBarang() {
-    System.out.println("\nId barang: " + this.id);
+    System.out.println("\nDetail Barang");
+    System.out.println("Id barang: " + this.id);
     System.out.println("Nama barang: " + this.name);
     System.out.println("Jumlah barang: " + this.qty);
+    System.out.println("Kategori barang: " + kategori);
   }
 }
